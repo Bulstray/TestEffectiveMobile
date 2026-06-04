a6c014f6c8fa0f2ce2e10108cb25cc5a41060616
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-04 15:33:41 +0300

    create func can this user see users

---
+++ fastapi-auth/dependencies/permissions.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper
+from core.shemas import UserRead
+from storage.db.user_settings.crud import get_settings_for_user
+
+from .auth import get_current_user
+
+
+async def can_this_user_see_users(
+    current_user: Annotated[
+        UserRead,
+        Depends(get_current_user),
+    ],
+    session: Annotated[
+        AsyncSession,
+        Depends(
+            db_helper.session_getter,
+        ),
+    ],
+) -> None:
+    user_settings = await get_settings_for_user(session, current_user.id)
+
+    if user_settings is not None and user_settings.can_see_users:
+        return
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="You don't have access to this resource",
+    )
