5b134ecb3bb52e9cca742cacee2edb53dc6e994a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-04 14:54:21 +0300

    create endpoint get_all_users, registration, login, logout, update, delete

---
+++ fastapi-auth/api/api_v1/users/auth.py
@@ -0,0 +1,99 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import User, db_helper
+from core.shemas import UserRead
+from storage.db.users import crud
+
+from .dependencies import (
+    create_new_account,
+    logout_system,
+    soft_delete_user,
+    update_user_profile,
+    validate_basic_auth_user,
+)
+
+router = APIRouter()
+
+
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=list[UserRead],
+)
+async def get_all_users(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+) -> list[User]:
+    return await crud.get_all_users(session)
+
+
+@router.post(
+    "/registration",
+    status_code=status.HTTP_201_CREATED,
+)
+async def registration(
+    token: Annotated[
+        str,
+        Depends(create_new_account),
+    ],
+) -> str:
+    return token
+
+
+@router.post(
+    "/login",
+    status_code=status.HTTP_200_OK,
+)
+async def login(
+    token_if_data_correct: Annotated[
+        str,
+        Depends(validate_basic_auth_user),
+    ],
+) -> str:
+    return token_if_data_correct
+
+
+@router.patch(
+    "/update",
+    status_code=status.HTTP_200_OK,
+)
+def update(
+    user_update: Annotated[
+        None,
+        Depends(update_user_profile),
+    ],
+) -> dict[str, str]:
+    return {
+        "message": "User updated",
+    }
+
+
+@router.delete("/delete_account")
+async def delete_account(
+    user_delete: Annotated[
+        None,
+        Depends(soft_delete_user),
+    ],
+) -> dict[str, str]:
+    return {
+        "message": "Account deleted",
+    }
+
+
+@router.post("/logout")
+async def logout(
+    user_logout: Annotated[
+        None,
+        Depends(
+            logout_system,
+        ),
+    ],
+) -> dict[str, str]:
+    return {
+        "message": "User logout",
+    }
