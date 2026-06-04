ce294d23c792e9f47d06bb4006729f422082781b
Ramil Khakimov <bulstray@yandex.ru>
2026-06-04 19:30:41 +0300

    include user settings update

--- fastapi-auth/core/shemas/__init__.py
+++ fastapi-auth/core/shemas/__init__.py
@@ -2,7 +2,9 @@ __all__ = (
     "UserLogin",
     "UserRead",
     "UserRegistration",
+    "UserSettingsUpdate",
     "UserUpdate",
 )
 
+from .user_setting import UserSettingsUpdate
 from .users import UserLogin, UserRead, UserRegistration, UserUpdate
