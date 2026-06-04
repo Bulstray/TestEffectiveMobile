2a67fbe14cc0d843640f551dd602f40d87ec06e3
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-04 15:40:08 +0300

    create user_settings

--- fastapi-auth/storage/db/user_settings/crud.py
+++ fastapi-auth/storage/db/user_settings/crud.py
@@ -14,3 +14,13 @@ async def get_settings_for_user(
 
     result = await session.execute(stmt)
     return result.scalar_one_or_none()
+
+
+async def create_user_settings(
+    session: AsyncSession,
+    user_id: int,
+) -> None:
+    session.add(
+        UserSettings(user_id=user_id),
+    )
+    await session.commit()
