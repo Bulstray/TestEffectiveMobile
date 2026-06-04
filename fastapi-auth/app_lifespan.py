4ec7e5db35d374acfc2e82314dccc4ae096ac29d
Ramil Khakimov <bulstray@yandex.ru>
2026-06-04 19:30:52 +0300

    create superuser

--- fastapi-auth/app_lifespan.py
+++ fastapi-auth/app_lifespan.py
@@ -3,11 +3,27 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from core.config import settings
 from core.models import db_helper
+from storage.db.user_settings.crud import create_user_settings
+from storage.db.users import crud
+
+
+async def create_admin() -> None:
+    async with db_helper.session_factory() as session:
+
+        if await crud.get_user_by_email(session, settings.superuser.email):
+            return
+
+        superuser = await crud.create_user(session, settings.superuser)
+
+        await create_user_settings(session, superuser.id)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+
+    await create_admin()
 
     yield
 
